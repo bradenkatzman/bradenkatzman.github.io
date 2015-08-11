d1864752efbfa7e3b94085351dffaac2437b89ea
bradenkatzman <bradenkatzman@gmail.com>
2015-08-11 12:37:12 -0700

    implemented jQuery string fading

---
+++ strings.js
@@ -0,0 +1,15 @@
+/* This file contains the array of strings which jquery uses on my homepage
+ * Author: Braden Katzman
+ */
+
+ var strings = [" a Computer Science student at Columbia University",
+ 				" double majoring",
+ 				" Jewish Philosophy",
+ 				" at The Jewish Theological Seminary of America",
+ 				" from",
+ 				" Northern California",
+ 				" likes",
+ 				" jQuery",
+ 				" is",
+ 				" in",
+ 				" a student"];
