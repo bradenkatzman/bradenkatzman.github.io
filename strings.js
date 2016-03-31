04c1d9afaaa6a940d418f393941e1dfc080b4c7a
Braden Katzman <bradenkatzman@gmail.com>
2016-03-31 19:05:24 -0400

    * complete update 3/31/16

--- strings.js
+++ strings.js
@@ -2,14 +2,44 @@
  * Author: Braden Katzman
  */
 
- var strings = [" studying Computer Science",
- 				" double majoring",
- 				" Jewish Philosophy",
- 				" from",
- 				" Northern California",
- 				" like",
- 				" jQuery",
- 				" am",
- 				" in",
- 				" a student at Columbia",
- 				"I"];
+ var strings = ["Hi", //0
+ 				"I'm", //1
+ 				" Braden", //2
+ 				" a", //3
+ 				"I ", //4
+ 				" student at Columbia University.", //5
+ 				" studying computer science.", //6
+ 				" Software Engineer at The Memorial Sloan Kettering Cancer Center.", //7
+ 				" interested in artificial intelligence.", //8
+ 				" Artifical Intelligence is my passion.", //9
+ 				" use AI to learn about biological processes.", //10
+ 				" like photography.", //11
+ 				" from the SF Bay Area.", //12
+ 				"Cooking soothes my soul.", //13
+ 				"Kant is my hero." //14
+ 				];
+
+
+/*
+Hi
+
+I'm Braden
+
+I'm a student at Columbia University
+
+I'm studying computer science
+
+I'm a Software Engineer at The Memorial Sloan Kettering Cancer Center
+
+Aritificial Intelligence is my passion
+
+I use AI to learn about biological processes
+
+I like photography
+
+I'm from the SF Bay Area
+
+Cooking soothes my soul
+
+Kant is my hero
+*/
